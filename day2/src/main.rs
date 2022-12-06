d50f6cb750186b149fb7ebc1812d75ec22416cb6
Vlad Ellis <me@ellisvlad.com>
2022-12-06 23:59:17 +0000

    Day2 Pt2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -5,6 +5,11 @@ enum Action {
     Rock, Paper, Scissors,
 }
 
+#[derive(Debug, PartialEq)]
+enum Strategy {
+    Win, Lose, Draw,
+}
+
 fn main() {
     let stdin = io::stdin();
     let mut sum = 0;
@@ -18,17 +23,44 @@ fn main() {
             _ => panic!("Unknown opponent input"),
         };
         line.next().unwrap();
-        let mine = match line.next().unwrap() {
-            'X' => Action::Rock,
-            'Y' => Action::Paper,
-            'Z' => Action::Scissors,
+        let strat = match line.next().unwrap() {
+            'X' => Strategy::Lose,
+            'Y' => Strategy::Draw,
+            'Z' => Strategy::Win,
             _ => panic!("Unknown user input"),
         };
+        let mine = calc_move(&opponent, &strat);
         let score = calc_score(&opponent, &mine);
         sum += score;
         println!("{:?} -> {:?} == {}", opponent, mine, score);
     }
     println!("Total: {}", sum);
+}
+
+fn calc_move(opponent: &Action, strat: &Strategy) -> Action {
+    match strat {
+        Strategy::Win => {
+            match opponent {
+                Action::Rock => Action::Paper,
+                Action::Paper => Action::Scissors,
+                Action::Scissors => Action::Rock,
+            }
+        },
+        Strategy::Lose => {
+            match opponent {
+                Action::Rock => Action::Scissors,
+                Action::Paper => Action::Rock,
+                Action::Scissors => Action::Paper,
+            }
+        },
+        Strategy::Draw => {
+            match opponent {
+                Action::Rock => Action::Rock,
+                Action::Paper => Action::Paper,
+                Action::Scissors => Action::Scissors,
+            }
+        }
+    }
 }
 
 fn calc_score(opponent: &Action, mine: &Action) -> i32 {
