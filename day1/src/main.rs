4e7fb318f622894de1d3853b9b028da0a541b905
Vlad Ellis <me@ellisvlad.com>
2022-12-06 23:27:04 +0000

    Day1 Pt2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,16 +3,21 @@ use std::io::{self, BufRead};
 fn main() {
     let stdin = io::stdin();
     let mut sum = 0;
-    let mut maximum_sum = 0;
+    let mut totals = Vec::new();
     for line in stdin.lock().lines() {
         let line = line.unwrap();
         if line.is_empty() {
             println!("{}", sum);
-            maximum_sum = if sum > maximum_sum {sum} else {maximum_sum};
+            totals.push(sum);
             sum = 0;
         } else {
             sum += line.parse::<i32>().unwrap();
         }
     }
-    println!("Maximum: {}", maximum_sum);
+    totals.sort();
+
+    println!("Top 3:");
+    println!("1. {}", totals.pop().unwrap());
+    println!("2. {}", totals.pop().unwrap());
+    println!("3. {}", totals.pop().unwrap());
 }
