c34c2464a60e8542e7aa9b37c394c5eb4e316282
Vlad Ellis <me@ellisvlad.com>
2022-12-07 00:37:26 +0000

    Day3 Pt2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,26 +3,45 @@ use std::{io::{self, BufRead}, collections::HashSet};
 fn main() {
     let stdin = io::stdin();
     let mut score_total = 0;
+    let mut triple_score_total = 0;
+    let mut triple_bag_builder = Vec::new();
     for line in stdin.lock().lines() {
         let line = line.unwrap();
         let len = line.len();
         let (first, second) = line.split_at(len / 2);
-        let common = find_first_common(first, second).unwrap();
-        let score = letter_score(&common);
+        let common = find_common(first, second);
+        let common = common.iter().next().unwrap();
+        let score = letter_score(common);
         score_total += score;
+        triple_bag_builder.push(line);
+        if triple_bag_builder.len() == 3 {
+            let common = find_common(
+                triple_bag_builder.get(0).unwrap(),
+                triple_bag_builder.get(1).unwrap());
+            let common = find_common(
+                &common.iter().map(|ch| ch.to_string()).collect::<Vec<String>>().join(""),
+                triple_bag_builder.get(2).unwrap());
+            for letter in common.iter() {
+                triple_score_total += letter_score(letter);
+            }
+            triple_bag_builder.clear();
+            println!("Triple common letter: {:?} ({triple_score_total})", common);
+        }
         println!("{common}: {score} ({score_total})");
     }
-    println!("Total: {score_total}")
+    println!("Total: {score_total}");
+    println!("Triple Total: {triple_score_total}");
 }
 
-fn find_first_common(first: &str, second: &str) -> Option<char> {
+fn find_common(first: &str, second: &str) -> HashSet<char> {
     let chars: HashSet<char> = first.chars().collect();
+    let mut common = HashSet::new();
     for ch in second.chars() {
         if chars.contains(&ch) {
-            return Some(ch)
+            common.insert(ch);
         }
     }
-    None
+    common
 }
 
 fn letter_score(letter: &char) -> i32 {
